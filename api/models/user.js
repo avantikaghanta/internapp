3c129cf9d30a6acc40f431c4f617e73f5bd59ac4
avantighanta <avantighanta@gmail.com>
2020-08-07 23:15:33 +0530

    intern app

--- api/models/user.js
+++ api/models/user.js
@@ -2,14 +2,40 @@
 const mongoose = require('mongoose');
 
 const userSchema =new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        unique:true
+    },
     email: { 
         type: String, 
         required: true, 
         unique: true, 
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    confirmpassword: {
+        type: String,
+        required: true,
+        min: 8
+    },
+    
+    team:
+        [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "team"
+      }],
+
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
